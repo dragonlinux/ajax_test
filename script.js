c6b6898e3189147dd19059e22e8f3ee75cfd208c
dragon <1018573693@qq.com>
2015-12-21 11:00:27 +0800

    init

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+    //document.getElementById("header").style.color = "red";
+    //document.write("hello");
+//    document.getElementById("p1").style.color = "blue";
+}, 3000)
